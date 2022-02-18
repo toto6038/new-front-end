eedcd2c8030b8a454b802847beffb93feb5b9cd6
TzuWei <ap9940506@gmail.com>
2022-02-19 03:34:00 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<style></style>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [require("daisyui")],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+}
