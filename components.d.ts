2c29e5b6ac70b6cc2680483d1978d0098f41e15f
TzuWei <ap9940506@gmail.com>
2022-02-22 02:07:11 +0800

    chore: add re-generated components.d.ts changes

--- components.d.ts
+++ components.d.ts
@@ -11,26 +11,16 @@ declare module 'vue' {
     HomeworkCard: typeof import('./src/components/HomeworkCard.vue')['default']
     IUilBars: typeof import('~icons/uil/bars')['default']
     IUilBookAlt: typeof import('~icons/uil/book-alt')['default']
-    IUilConstructor: typeof import("~icons/uil/constructor")["default"];
     IUilFacebook: typeof import('~icons/uil/facebook')['default']
     IUilGithub: typeof import('~icons/uil/github')['default']
     IUilHome: typeof import('~icons/uil/home')['default']
-    IUilInfCircle: typeof import("~icons/uil/inf-circle")["default"];
-    IUilInfoCircle: typeof import("~icons/uil/info-circle")["default"];
-    IUilInfpCircle: typeof import("~icons/uil/infp-circle")["default"];
-    IUilInfpoCircle: typeof import("~icons/uil/infpo-circle")["default"];
     IUilMapMarkerInfo: typeof import('~icons/uil/map-marker-info')['default']
-    IUilMapMarkInfo: typeof import("~icons/uil/map-mark-info")["default"];
     IUilMoon: typeof import('~icons/uil/moon')['default']
-    IUilNewspaper: typeof import("~icons/uil/newspaper")["default"];
-    IUilNinja: typeof import("~icons/uil/ninja")["default"];
     IUilSun: typeof import('~icons/uil/sun')['default']
     IUilUser: typeof import('~icons/uil/user')['default']
-    IUilUserCircle: typeof import("~icons/uil/user-circle")["default"];
     IUilWrench: typeof import('~icons/uil/wrench')['default']
     JudgeStatus: typeof import('./src/components/JudgeStatus.vue')['default']
     MarkdownRenderer: typeof import('./src/components/MarkdownRenderer.vue')['default']
-    NavBar: typeof import("./src/components/NavBar.vue")["default"];
     PostCard: typeof import('./src/components/PostCard.vue')['default']
     SampleCodeBlock: typeof import('./src/components/SampleCodeBlock.vue')['default']
     SideBar: typeof import('./src/components/SideBar.vue')['default']
