b52c2acfde2b5dc755cf08de4ca08be5c8d62a42
TzuWei <ap9940506@gmail.com>
2022-03-23 02:26:45 +0800

    add sentry

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,24 @@ import App from "./App.vue";
 import router from "./router";
 import "./index.css";
 import "vue-prism-editor/dist/prismeditor.min.css";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: "https://de0e8c6700ff429ba4122b05cc21d520@o876599.ingest.sentry.io/6271638",
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["localhost", "v2.noj.tw", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.5,
+});
+
+app.use(createPinia()).use(router).mount("#app");
