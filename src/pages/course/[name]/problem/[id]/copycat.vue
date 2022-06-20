0aba199387b7b6a8b007c0a525a39fbf000e27f3
TzuWei <ap9940506@gmail.com>
2022-06-20 23:38:42 +0800

    improving copycat ui (still bad)

--- src/pages/course/[name]/problem/[id]/copycat.vue
+++ src/pages/course/[name]/problem/[id]/copycat.vue
@@ -2,16 +2,24 @@
 import { useAxios } from "@vueuse/integrations/useAxios";
 import { useRoute } from "vue-router";
 import api, { fetcher } from "../../../../../models/api";
-import { useTitle } from "@vueuse/core";
+import { useIntervalFn, useTitle } from "@vueuse/core";
+import { watchEffect } from "vue";
+import { computed } from "@vue/reactivity";
 
 const route = useRoute();
 useTitle(`Copycat - ${route.params.id} - ${route.params.name} | Normal OJ`);
-const { data, error, isLoading } = useAxios(
+const { data: course } = useAxios(`/course/${route.params.name}`, fetcher);
+const { data, error, execute } = useAxios(
   `/copycat?course=${route.params.name}&problemId=${route.params.id}`,
   fetcher,
 );
-const { data: course } = useAxios(`/course/${route.params.name}`, fetcher);
-
+const isReportGenerated = computed<boolean>(() => data.value && Object.values(data.value).some(Boolean));
+const { pause, resume } = useIntervalFn(execute, 10000);
+watchEffect(() => {
+  if (isReportGenerated) {
+    pause();
+  }
+});
 async function detect() {
   const studentNicknames = Object.fromEntries(
     course.value.students.map((student: any) => [student.username, student.displayedName]),
@@ -23,9 +31,8 @@ async function detect() {
   };
   try {
     await api.Copycat.detect(body);
-  } catch {
-  } finally {
-  }
+    resume();
+  } catch {}
 }
 </script>
 
@@ -35,9 +42,12 @@ async function detect() {
       <div class="card-body">
         <div class="card-title md:text-2xl lg:text-3xl">Copycat of problem #{{ $route.params.id }}</div>
 
-        <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+        <div v-if="!isReportGenerated">No report.</div>
+        <div v-else>Report generating...</div>
 
         <div class="btn" @click="detect"><i-uil-file-upload-alt class="mr-1 h-5 w-5" /> Detect</div>
+
+        <div v-if="data" v-html="data.cpp_report" />
       </div>
     </div>
   </div>
