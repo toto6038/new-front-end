a3c3d84399d0ef960799ff33b4f3051c05c23da5
Misawai <69676474+Misawai@users.noreply.github.com>
2022-12-05 22:02:53 +0800

    update 'about' part of the interface.

--- src/i18n/chinese.ts
+++ src/i18n/chinese.ts
@@ -4,6 +4,12 @@ export default {
     select_lang: "選擇語言",
   },
   profile: {},
-  about: {},
+  about: {
+    contributor:"貢獻者",
+    links:"連結",
+    maintainer:"維護人員",
+    service_l_2021: "臺師大資工系2021年春季服務學習",
+    swe_2019: "2019年秋季軟體工程「紅隊」組",
+  },
   course: {},
 };
--- src/i18n/english.ts
+++ src/i18n/english.ts
@@ -4,6 +4,12 @@ export default {
     select_lang: "Select your preferred language",
   },
   profile: {},
-  about: {},
+  about: {
+    contributor:"Contributor",
+    links:"Links",
+    maintainer:"Maintainer",
+    service_l_2021: "NTNU CSIE Service Learning 2021 Spring",
+    swe_2019: "Red Team of Software Engineering 2019 Fall",
+  },
   course: {},
 };
