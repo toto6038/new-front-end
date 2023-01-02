cb02bdf44d1b1931215c1a876bdbd1d6bab05369
TzuWei <ap9940506@gmail.com>
2023-01-03 01:03:55 +0800

    fix(homework.stats): tweak the text & bg color in scoreboard cell & fixed 2 fractional digits for avg score
    close 11

--- src/pages/course/[name]/homeworks/[id]/stats.vue
+++ src/pages/course/[name]/homeworks/[id]/stats.vue
@@ -81,8 +81,8 @@ const sortedScoreboard = computed(() => {
 function getCellColor(cell: ScoreCell | undefined) {
   if (!cell) return "";
   if (cell.count === 0) return "bg-gray-200";
-  if (cell.max < 100) return "bg-red-200";
-  return "bg-green-200";
+  if (cell.max < 100) return "bg-red-300 text-neutral";
+  return "bg-green-300 text-neutral";
 }
 const scoreDistribution = computed(() => {
   if (!scoreboard.value) return {};
@@ -221,7 +221,7 @@ function exportCSV() {
                   <div class="text-xs">{{ row[`${pid}`].count }} tries</div>
                 </div>
               </td>
-              <td class="text-center">{{ row.avg }}</td>
+              <td class="text-center">{{ row.avg.toFixed(2) }}</td>
               <td class="text-center">{{ row.sum }}</td>
             </tr>
           </tbody>
