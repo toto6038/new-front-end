511c5d8db0260d9d95b87bd3b72cc7e879772f11
Howard Guo <50100922+toto6038@users.noreply.github.com>
2023-01-14 04:20:44 +0800

    chore: add some comments for vite type definition

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,5 @@
+// This line is used to provide type definition of VITE env variables
+// See https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client" />
 
