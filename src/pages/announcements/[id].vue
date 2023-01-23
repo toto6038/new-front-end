b9211ae2f2cfc3db846f2219401009c389d97c72
Misawai <69676474+Misawai@users.noreply.github.com>
2023-01-23 16:54:46 +0800

    feat(id.vue): new translation finished

--- src/pages/announcements/[id].vue
+++ src/pages/announcements/[id].vue
@@ -3,8 +3,11 @@ import { useTitle } from "@vueuse/core";
 import { useAxios } from "@vueuse/integrations/useAxios";
 import { useRoute } from "vue-router";
 import { fetcher } from "@/models/api";
+import { useI18n } from "vue-i18n";
 
 const route = useRoute();
+const { t, locale } = useI18n();
+
 useTitle(`Announcement - ${route.params.id} | Normal OJ`);
 const {
   data: announcements,
@@ -16,7 +19,7 @@ const {
 <template>
   <div class="mx-auto flex max-w-7xl gap-8 p-4">
     <router-link class="btn-sm btn mb-10" to="/">
-      <i-uil-left-arrow-to-left class="mr-1" /> Back
+      <i-uil-left-arrow-to-left class="mr-1" /> {{ t("ann.id.back") }}
     </router-link>
 
     <data-status-wrapper :error="error" :is-loading="isLoading">
