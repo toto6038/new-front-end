265cc2814c85102bc283f4f649f25c3a60aab056
James Lo <69676474+Misawai@users.noreply.github.com>
2023-01-31 14:57:23 +0800

    feat: misawai's new changes after rebase (#51)
    Co-authored-by: TzuWei <ap9940506@gmail.com>

--- src/i18n/chinese.ts
+++ src/i18n/chinese.ts
@@ -4,13 +4,50 @@ export default {
     select_lang: "選擇語言",
   },
   about: {
-    contributor:"貢獻者",
-    links:"連結",
-    maintainer:"維護人員",
+    contributor: "貢獻者",
+    links: "連結",
+    maintainer: "維護人員",
     service_l_2021: "臺師大資工系2021年春季服務學習",
     swe_2019: "2019年秋季軟體工程「紅隊」組",
   },
-  course: {},
+  course: {
+    ann: {
+      index: {
+        title: "公告",
+        err: "Oops! Something went wrong when loading announcements.",
+        table: {
+          title: "標題",
+          author: "發布者",
+          time: "發布時間",
+        },
+      },
+    },
+    hw: {
+      index: {
+        title: "作業",
+        new: "新增",
+        err: "Oops! Something went wrong when loading homeworks.",
+      },
+    },
+    problem: {
+      submit: {
+        err: {
+          code: "請在此貼上你的原始碼以利提交。",
+          lang: "請選擇程式語言。",
+          msg: "Oops! Something went wrong when submitting the code. Try again later.",
+        },
+        card: {
+          title: "提交至題目 #",
+          placeholder: "在此貼上程式碼",
+        },
+        lang: {
+          text: "語言",
+          select: "選擇語言",
+        },
+        text: "提交",
+      },
+    },
+  },
   index: {
     ann: "公告",
     error: "噢不！載入公告時出現了技術錯誤。",
@@ -25,8 +62,8 @@ export default {
     },
   },
   profile: {
-    title : "個人資料",
-    err:{
+    title: "個人資料",
+    err: {
       new_pw: "請填寫此欄位。",
       old_pw: "請填寫此欄位。",
       confirm_pw: {
@@ -40,7 +77,7 @@ export default {
     role: "角色",
     pw: {
       change: "變更密碼",
-      change_msg:  "密碼已成功變更",
+      change_msg: "密碼已成功變更",
       new: "新密碼",
       confirm: "確認新密碼",
       current: "舊密碼",
@@ -50,6 +87,11 @@ export default {
         current: "舊密碼",
       },
       submit: "送出",
+    },
+  },
+  ann: {
+    id: {
+      back: "返回",
     },
   },
 };
