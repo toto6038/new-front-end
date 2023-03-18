f81f9074f9cfafcc7dff55423b30380ff0ae0944
JacobLinCool <jacoblincool@gmail.com>
2023-03-18 21:01:57 +0800

    feat: display the copy button when hovering the code block (#87)

--- src/components/SampleCodeBlock.vue
+++ src/components/SampleCodeBlock.vue
@@ -12,9 +12,9 @@ const { copy, copied, isSupported } = useClipboard();
 </script>
 
 <template>
-  <div class="relative">
+  <div class="relative group">
     <pre class="rounded bg-zinc-800 py-2 px-4 text-white"><code>{{ code }}</code></pre>
-    <button v-if="isSupported" class="btn btn-info btn-xs absolute right-1 top-1" @click="copy(code)">
+    <button v-if="isSupported" class="btn btn-info btn-xs absolute right-1 top-1 hidden group-hover:inline-block" @click="copy(code)">
       {{ copied ? "Copied!" : "Copy" }}
     </button>
   </div>
